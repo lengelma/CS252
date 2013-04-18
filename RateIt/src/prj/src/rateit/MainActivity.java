ae45f7e9dd328250a4326dd9364b6ac8c5ff3e55
shah117 <shah117@purdue.edu>
2013-04-18 19:20:48 -0400

    RateIt User Interface

---
+++ RateIt/src/prj/src/rateit/MainActivity.java
@@ -0,0 +1,35 @@
+package prj.src.rateit;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    public void ownersFrame(View view){
+    Intent intent = new Intent(this, OwnersLogin.class);	
+    startActivity(intent);	
+    }
+    
+    public void customersFrame(View view){
+    	
+    	
+    }
+        
+}
